77d5d3a067a41b4adadda25be3d2fb22c44f2222
mayank.jain1 <mayank.jain1@appdynamics.com>
2022-05-09 17:02:13 +0530

    Spring wired with explicit java config

---
+++ src/main/java/com/cisco/training/dal/ProductDAO.java
@@ -0,0 +1,16 @@
+package com.cisco.training.dal;
+//data access layer
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cisco.training.domain.Product;
+
+public interface ProductDAO {
+
+	public Product save(Product toBeSaved);
+	public Optional<Product> findById(int id);
+	public List<Product> findAll();
+	public void deleteById(int id);
+	
+}
